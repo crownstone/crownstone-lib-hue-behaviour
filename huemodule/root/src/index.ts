0c0571193f776eabd2fa9c0cee2774f138e3a720
Joey Ouwens <joey.ouwens@gmail.com>
2020-09-14 13:16:19 +0200

    Revert "Index only handles imports."
    This reverts commit 952a66a3e000d5b1c400b3a8915b60bcb5f858c6.

--- huemodule/root/src/index.ts
+++ huemodule/root/src/index.ts
@@ -1,3 +1,15 @@
+import {Framework} from "./Framework";
+import { Bridge } from "./Bridge";
+
 export {Light} from "./Light";
 export {Framework} from "./Framework";
 export {Bridge} from "./Bridge"
+
+async function testing(){
+    const framework = new Framework();
+    await framework.init();
+    const notWorkingBridge = new Bridge("Hue color lamp 3","user","key","mac","192.168.178.12","-1",framework);
+    console.log(await notWorkingBridge.init());
+}
+
+testing();
