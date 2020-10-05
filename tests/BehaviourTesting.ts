4802a757c2fe504af16a18d9e26e98a358c3aaea
Joey Ouwens <joey.ouwens@gmail.com>
2020-10-05 17:23:23 +0200

    Behaviour unit tests

--- tests/BehaviourTesting.ts
+++ tests/BehaviourTesting.ts
@@ -1,99 +1,99 @@
-// Deze hoort niet in de src
-
-
-
-
-import {Bridge} from "./index";
-import {Framework} from "./index";
-import {BehaviourModule} from "./BehaviourModule";
-import {Light} from "./index";
-import {setLightState} from "node-hue-api/lib/api/http/endpoints/lights";
-import {Behaviour} from "./Behaviour";
-import {EventBus} from "./util/EventBus";
-const eventBus = new EventBus();
-
-let SimulatedPersonIsInRoom = true;
-const BehaviourWrapperSim = {
-    "type": "BEHAVIOUR",
-    "data": {
-        "action": {"type": "BE_ON", "data": 100},
-        "time": {
-            "type": "RANGE",
-            "from": {"type": "CLOCK", "data": {"hours": 10, "minutes": 10}},
-            "to": {"type": "CLOCK", "data":{ "hours": 13, "minutes": 10}}
-        },
-        "presence": {"type": "SOMEBODY", "data":{"type":"SPHERE"} }
-    },
-    "idOnCrownstone": 0,
-    "profileIndex": 0,
-    "syncedToCrownstone": true,
-    "deleted": false,
-    "activeDays": {
-        "Mon": true,
-        "Tue": true,
-        "Wed": true,
-        "Thu": true,
-        "Fri": true,
-        "Sat": true,
-        "Sun": true
-    },
-    "id": "5f649e889280050004952618",
-    "stoneId": "5f4e47660bc0da0004b4fe16",
-    "sphereId": "5f4d08bbbfeb1e000422a462",
-    "createdAt": "2020-09-18T11:48:24.813Z",
-    "updatedAt": "2020-09-18T11:48:24.489Z"
-}
-
-let simulatedBehaviours = {};
-const framework = new Framework();
-const module = new BehaviourModule();
-let lights = [];
-async function simulation() {
-    const behaviour = new Behaviour(BehaviourWrapperSim,eventBus);
-
-    let date = new Date(2020,8 ,30,20,10,0);
-    behaviour.tick(Date.parse(date.toISOString()))
-
-    console.log(behaviour.isActive);
-
-
-    // await module.init();
-
-    // module.addBehaviour(BehaviourWrapperSim,"Bedroom");
-
-    // const mod = module._loop();
-    // await cycle(500);
-    // await mod;
-}
-
-
-const cycle = async (ms) => {
-    try {
-        await simulatePresence();
-        await delay(ms);
-
-        return cycle(ms);
-    } catch(err){
-        console.log(err);
-        return cycle(ms);
-
-    }
-};
-
-const delay = ms => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-};
-
-
-simulation();
-
-
-async function simulatePresence() {
-    if (Math.floor(Math.random() * Math.floor(15)) === 2) {
-        console.log("Person walks " + ((SimulatedPersonIsInRoom)?"into":"out of") + " the room" )
-        module.detectPresence({room: "Bedroom", presence: SimulatedPersonIsInRoom});
-        SimulatedPersonIsInRoom = !SimulatedPersonIsInRoom;
-        await delay(500);
-
-    }
-}
+// // Deze hoort niet in de src
+//
+//
+//
+//
+// import {Bridge} from "./index";
+// import {Framework} from "./index";
+// import {BehaviourModule} from "./BehaviourModule";
+// import {Light} from "./index";
+// import {setLightState} from "node-hue-api/lib/api/http/endpoints/lights";
+// import {Behaviour} from "./Behaviour";
+// import {EventBus} from "./util/EventBus";
+// const eventBus = new EventBus();
+//
+// let SimulatedPersonIsInRoom = true;
+// const BehaviourWrapperSim = {
+//     "type": "BEHAVIOUR",
+//     "data": {
+//         "action": {"type": "BE_ON", "data": 100},
+//         "time": {
+//             "type": "RANGE",
+//             "from": {"type": "CLOCK", "data": {"hours": 10, "minutes": 10}},
+//             "to": {"type": "CLOCK", "data":{ "hours": 13, "minutes": 10}}
+//         },
+//         "presence": {"type": "SOMEBODY", "data":{"type":"SPHERE"} }
+//     },
+//     "idOnCrownstone": 0,
+//     "profileIndex": 0,
+//     "syncedToCrownstone": true,
+//     "deleted": false,
+//     "activeDays": {
+//         "Mon": true,
+//         "Tue": true,
+//         "Wed": true,
+//         "Thu": true,
+//         "Fri": true,
+//         "Sat": true,
+//         "Sun": true
+//     },
+//     "id": "5f649e889280050004952618",
+//     "stoneId": "5f4e47660bc0da0004b4fe16",
+//     "sphereId": "5f4d08bbbfeb1e000422a462",
+//     "createdAt": "2020-09-18T11:48:24.813Z",
+//     "updatedAt": "2020-09-18T11:48:24.489Z"
+// }
+//
+// let simulatedBehaviours = {};
+// const framework = new Framework();
+// const module = new BehaviourModule();
+// let lights = [];
+// async function simulation() {
+//     const behaviour = new Behaviour(BehaviourWrapperSim,eventBus);
+//
+//     let date = new Date(2020,8 ,30,20,10,0);
+//     behaviour.tick(Date.parse(date.toISOString()))
+//
+//     console.log(behaviour.isActive);
+//
+//
+//     // await module.init();
+//
+//     // module.addBehaviour(BehaviourWrapperSim,"Bedroom");
+//
+//     // const mod = module._loop();
+//     // await cycle(500);
+//     // await mod;
+// }
+//
+//
+// const cycle = async (ms) => {
+//     try {
+//         await simulatePresence();
+//         await delay(ms);
+//
+//         return cycle(ms);
+//     } catch(err){
+//         console.log(err);
+//         return cycle(ms);
+//
+//     }
+// };
+//
+// const delay = ms => {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+// };
+//
+//
+// simulation();
+//
+//
+// async function simulatePresence() {
+//     if (Math.floor(Math.random() * Math.floor(15)) === 2) {
+//         console.log("Person walks " + ((SimulatedPersonIsInRoom)?"into":"out of") + " the room" )
+//         module.detectPresence({room: "Bedroom", presence: SimulatedPersonIsInRoom});
+//         SimulatedPersonIsInRoom = !SimulatedPersonIsInRoom;
+//         await delay(500);
+//
+//     }
+// }
--- tests/behaviours.test.ts
+++ tests/behaviours.test.ts
@@ -6,9 +6,8 @@ import {eventBus} from "../src/util/EventBus";
 const Behaviour = require('../src/behaviour/behaviour/Behaviour').Behaviour
 const BehaviourSupport = require('../src/behaviour/behaviour/BehaviourSupport').BehaviourSupport
 const BehaviourUtil = require('../src/behaviour/behaviour/BehaviourUtil')
-const sphereLocation = {
-  latitude: 51.916064, longitude: 4.472683
-}
+
+const SPHERE_LOCATION = {latitude: 51.916064, longitude: 4.472683} // Rotterdam
 const PRESENCE_DETECT_TOPIC = "onPresenceDetect"
 const EVENT_ENTER_SPHERE = {type: "ENTER", data: {type: "SPHERE", profileIdx: 0}}
 const EVENT_ENTER_LOCATION = {type: "ENTER", data: {type: "LOCATION", profileIdx: 0, locationId: 1}}
@@ -22,7 +21,7 @@ describe("End Condition testing", () => {
   test('Behaviour should be active when user is still in sphere as End condition, activation time expired.', () => {
     const behaviourSupport = new BehaviourSupport()
     behaviourSupport.setTimeFromClock(9, 10).setTimeToClock(16, 10).setDimPercentage(90).setPresenceIgnore().setEndConditionWhilePeopleInSphere();
-    const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
+    const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
     const time = new Date()
     time.setHours(9);
     time.setMinutes(10);
@@ -37,7 +36,7 @@ describe("End Condition testing", () => {
   test('Behaviour should be inactive when user leaves sphere as End condition, activation time expired.', () => {
     const behaviourSupport = new BehaviourSupport()
     behaviourSupport.setTimeFromClock(9, 10).setTimeToClock(16, 10).setDimPercentage(90).setPresenceIgnore().setEndConditionWhilePeopleInSphere();
-    const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
+    const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
     const time = new Date()
     time.setHours(9);
     time.setMinutes(10);
@@ -54,7 +53,7 @@ describe("Location testing", () => {
   test('Behaviour should be active when user is in location.', () => {
     const behaviourSupport = new BehaviourSupport()
     behaviourSupport.setTimeAllDay().setDimPercentage(90).setPresenceSomebodyInLocations([1]);
-    const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
+    const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
     eventBus.emit(PRESENCE_DETECT_TOPIC, EVENT_ENTER_LOCATION);
     behaviour.tick(Date.now());
     return expect(behaviour.isActive).toBeTruthy();
@@ -62,26 +61,27 @@ describe("Location testing", () => {
   test('Behaviour should be inactive when user is not in location.', () => {
     const behaviourSupport = new BehaviourSupport()
     behaviourSupport.setTimeAllDay().setDimPercentage(90).setPresenceSomebodyInLocations([1]);
-    const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
+    const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
     behaviour.tick(Date.now());
     return expect(behaviour.isActive).toBeFalsy();
   });
   test('Behaviour should be active when multiple users are in location and one left.', () => {
     const behaviourSupport = new BehaviourSupport()
     behaviourSupport.setTimeAllDay().setDimPercentage(90).setPresenceSomebodyInLocations([1]);
-    const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
+    const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
     eventBus.emit(PRESENCE_DETECT_TOPIC, EVENT_ENTER_LOCATION);
     eventBus.emit(PRESENCE_DETECT_TOPIC, EVENT_ENTER_LOCATION);
+    eventBus.emit(PRESENCE_DETECT_TOPIC, EVENT_ENTER_LOCATION_TWO);
     behaviour.tick(Date.now());
-    eventBus.emit(PRESENCE_DETECT_TOPIC, EVENT_LEAVE_LOCATION);
+    eventBus.emit(PRESENCE_DETECT_TOPIC, EVENT_LEAVE_LOCATION)
     behaviour.tick(Date.now());
     return expect(behaviour.isActive).toBeTruthy();
   });
   test('Behaviour should be active when nobody is in location.', () => {
     const behaviourSupport = new BehaviourSupport()
     behaviourSupport.setTimeAllDay().setDimPercentage(90).setPresenceNobodyInLocations([1]);
-    const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
-    behaviour.tick(Date.parse(new Date(2020,9,5,10,10).toString()));
+    const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
+    behaviour.tick(Date.parse(new Date(2020, 9, 5, 10, 10).toString()));
     return expect(behaviour.isActive).toBeTruthy();
   });
 });
@@ -90,7 +90,7 @@ describe("Sphere testing", () => {
   test('Behaviour should be active when user is in sphere.', () => {
     const behaviourSupport = new BehaviourSupport()
     behaviourSupport.setTimeAllDay().setDimPercentage(90).setPresenceSomebodyInSphere();
-    const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
+    const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
     eventBus.emit(PRESENCE_DETECT_TOPIC, EVENT_ENTER_SPHERE);
     behaviour.tick(Date.now());
     return expect(behaviour.isActive).toBeTruthy();
@@ -98,14 +98,14 @@ describe("Sphere testing", () => {
   test('Behaviour should be inactive when user is not in sphere.', () => {
     const behaviourSupport = new BehaviourSupport()
     behaviourSupport.setTimeAllDay().setDimPercentage(90).setPresenceSomebodyInSphere();
-    const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
+    const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
     behaviour.tick(Date.now());
     return expect(behaviour.isActive).toBeFalsy();
   });
   test('Behaviour should be active when multiple users are in sphere and one left.', () => {
     const behaviourSupport = new BehaviourSupport()
     behaviourSupport.setTimeAllDay().setDimPercentage(90).setPresenceSomebodyInSphere();
-    const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
+    const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
     eventBus.emit(PRESENCE_DETECT_TOPIC, EVENT_ENTER_SPHERE);
     eventBus.emit(PRESENCE_DETECT_TOPIC, EVENT_ENTER_SPHERE);
     behaviour.tick(Date.now());
@@ -117,7 +117,7 @@ describe("Sphere testing", () => {
   test('Behaviour should be active when nobody is in sphere.', () => {
     const behaviourSupport = new BehaviourSupport()
     behaviourSupport.setTimeAllDay().setDimPercentage(90).setPresenceNobodyInSphere();
-    const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
+    const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
     behaviour.tick(Date.now());
     return expect(behaviour.isActive).toBeTruthy();
   });
@@ -125,7 +125,7 @@ describe("Sphere testing", () => {
   test('Behaviour should be inactive when somebody is in sphere.', () => {
     const behaviourSupport = new BehaviourSupport()
     behaviourSupport.setTimeAllDay().setDimPercentage(90).setPresenceNobodyInSphere();
-    const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
+    const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
     eventBus.emit(PRESENCE_DETECT_TOPIC, EVENT_ENTER_SPHERE);
     behaviour.tick(Date.now());
     return expect(behaviour.isActive).toBeFalsy();
@@ -137,7 +137,7 @@ describe('Delay testing', () => {
     test('Behaviour should be active when user just left sphere', () => {
       const behaviourSupport = new BehaviourSupport()
       behaviourSupport.setTimeAllDay().setDimPercentage(90).setPresenceSomebodyInSphere();
-      const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
+      const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
       const time = new Date()
       time.setHours(0);
       time.setMinutes(10);
@@ -154,7 +154,7 @@ describe('Delay testing', () => {
     test('Behaviour should be inactive when user left location and delay period is exceeded.', () => {
       const behaviourSupport = new BehaviourSupport()
       behaviourSupport.setTimeAllDay().setDimPercentage(90).setPresenceSomebodyInSphere(); // 5 Minutes delay per default.
-      const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
+      const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
       eventBus.emit(PRESENCE_DETECT_TOPIC, EVENT_ENTER_LOCATION);
       const time = new Date()
       time.setHours(0);
@@ -171,7 +171,7 @@ describe('Delay testing', () => {
     test('Behaviour should be inactive when user just left the sphere', () => {
       const behaviourSupport = new BehaviourSupport()
       behaviourSupport.setTimeAllDay().setDimPercentage(90).setPresenceNobodyInSphere();
-      const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
+      const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
       const time = new Date()
       time.setHours(0);
       time.setMinutes(10);
@@ -187,7 +187,7 @@ describe('Delay testing', () => {
     test('Behaviour should be active when user left the sphere and timeout expired', () => {
       const behaviourSupport = new BehaviourSupport()
       behaviourSupport.setTimeAllDay().setDimPercentage(90).setPresenceNobodyInSphere();
-      const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
+      const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
       const time = new Date()
       time.setHours(0);
       time.setMinutes(10);
@@ -215,7 +215,7 @@ describe('Time testing', () => {
         Sat: false,
         Sun: true
       });
-      const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
+      const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
       const time = new Date(2020, 9, 4, 10, 0);
       behaviour.tick(Date.parse(time.toString()));
       return expect(behaviour.isActive).toBeTruthy();
@@ -231,7 +231,7 @@ describe('Time testing', () => {
         Sat: false,
         Sun: true
       });
-      const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
+      const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
       const time = new Date(2020, 9, 5, 3, 59);
       behaviour.tick(Date.parse(time.toString()));
       return expect(behaviour.isActive).toBeTruthy();
@@ -247,7 +247,7 @@ describe('Time testing', () => {
         Sat: false,
         Sun: true
       });
-      const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
+      const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
       const time = new Date(2020, 9, 5, 4, 0);
       behaviour.tick(Date.parse(time.toString()));
       return expect(behaviour.isActive).toBeFalsy();
@@ -257,71 +257,94 @@ describe('Time testing', () => {
 
   describe('Range testing', () => {
     describe('Sunset, Sunrise testing', () => {
-
-      test('Behaviour should be active when user is in location and activation between sunset -> sunrise.', () => {
+      test('Behaviour should be active when activation is between sunset -> sunrise.', () => {
         const behaviourSupport = new BehaviourSupport()
-        behaviourSupport.setTimeFromSunset().setTimeToSunrise().setDimPercentage(90).setPresenceSomebodyInLocations([1]);
-        const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
-        const time = new Date()
-        time.setHours(0);
-        time.setMinutes(10);
-        eventBus.emit(PRESENCE_DETECT_TOPIC, EVENT_ENTER_LOCATION);
+        behaviourSupport.setTimeFromSunset().setTimeToSunrise().setDimPercentage(90).ignorePresence();
+        const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
+        const time = new Date(2020, 9, 5, 19, 12);
         behaviour.tick(Date.parse(time.toString()));
         return expect(behaviour.isActive).toBeTruthy();
       });
 
-      test('Behaviour should be active when user is not in location and activation between sunset -> sunrise.', () => {
+      test('Behaviour should be active when activation between sunrise +10 offset -> sunset.', () => {
         const behaviourSupport = new BehaviourSupport()
-        behaviourSupport.setTimeFromSunset().setTimeToSunrise().setDimPercentage(90).setPresenceSomebodyInLocations([1]);
-        const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
-        const time = new Date()
-        time.setHours(0);
-        time.setMinutes(10);
+        behaviourSupport.setTimeToSunset().setTimeFromSunrise(10).setDimPercentage(90).ignorePresence();
+        const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
+        const time = new Date(2020, 9, 5, 8, 4);
+        behaviour.tick(Date.parse(time.toString()));
+        return expect(behaviour.isActive).toBeTruthy();
+      });
+      test('Behaviour should be active when  time between sunrise -10 offset -> sunset.', () => {
+        const behaviourSupport = new BehaviourSupport()
+        behaviourSupport.setTimeToSunset().setTimeFromSunrise(-10).setDimPercentage(90).ignorePresence();
+        const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
+        const time = new Date(2020, 9, 5, 7, 44);
+        behaviour.tick(Date.parse(time.toString()));
+        return expect(behaviour.isActive).toBeTruthy();
+      });
+      test('Behaviour should be inactive when time is before start time, sunrise  -> sunset.', () => {
+        const behaviourSupport = new BehaviourSupport()
+        behaviourSupport.setTimeToSunset().setTimeFromSunrise().setDimPercentage(90).ignorePresence();
+        const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
+        const time = new Date(2020, 9, 5, 7, 53);
+        behaviour.tick(Date.parse(time.toString()));
+        return expect(behaviour.isActive).toBeFalsy();
+      });
+      test('Behaviour should be inactive when time is after sunset end time, sunrise -> sunset + 10 offset.', () => {
+        const behaviourSupport = new BehaviourSupport()
+        behaviourSupport.setTimeToSunset(+10).setTimeFromSunrise().setDimPercentage(90).ignorePresence();
+        const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
+        const time = new Date(2020, 9, 5, 19, 22);
+        behaviour.tick(Date.parse(time.toString()));
+        return expect(behaviour.isActive).toBeFalsy();
+      });
+    });
+
+    describe('Clock testing',()=>{
+      test('Behaviour should be active when activation is between 22:00 -> 23:59, Time 22:00.', () => {
+        const behaviourSupport = new BehaviourSupport()
+        behaviourSupport.setTimeFromClock(22,0).setTimeToClock(23,59).setDimPercentage(90).ignorePresence();
+        const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
+        const time = new Date(2020, 9, 5, 22, 0);
+        behaviour.tick(Date.parse(time.toString()));
+        return expect(behaviour.isActive).toBeTruthy();
+      });
+
+      test('Behaviour should be active when activation is between 22:00 -> 6:59, Time 6.58.', () => {
+        const behaviourSupport = new BehaviourSupport()
+        behaviourSupport.setTimeFromClock(22,0).setTimeToClock(6,59).setDimPercentage(90).ignorePresence().setActiveDays({
+          Mon: false,
+          Tue: false,
+          Wed: false,
+          Thu: false,
+          Fri: false,
+          Sat: false,
+          Sun: true
+        });;
+        const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
+        const time = new Date(2020, 9, 5, 6, 58);
+        behaviour.tick(Date.parse(time.toString()));
+        return expect(behaviour.isActive).toBeTruthy();
+      });
+
+      test('Behaviour should be inactive when time is after end time, 22:00 -> 6:59', () => {
+        const behaviourSupport = new BehaviourSupport()
+        behaviourSupport.setTimeFromClock(22,0).setTimeToClock(6,59).setDimPercentage(90).ignorePresence().setActiveDays({
+          Mon: false,
+          Tue: false,
+          Wed: false,
+          Thu: false,
+          Fri: false,
+          Sat: false,
+          Sun: true
+        });;
+        const behaviour = new Behaviour(behaviourSupport.rule, SPHERE_LOCATION);
+        const time = new Date(2020, 9, 5, 6, 59);
         behaviour.tick(Date.parse(time.toString()));
         return expect(behaviour.isActive).toBeFalsy();
       });
 
-      test('Behaviour should be active when user is in sphere and activation between sunrise +10 offset -> sunset.', () => {
-        const behaviourSupport = new BehaviourSupport()
-        behaviourSupport.setTimeToSunset().setTimeFromSunrise(10).setDimPercentage(90).setPresenceSomebodyInSphere();
-        const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
-        const time = new Date(2020,10,5,7,59);
-        behaviour.tick(Date.parse(time.toString()));
-        eventBus.emit(PRESENCE_DETECT_TOPIC, EVENT_ENTER_SPHERE);
-        behaviour.tick(Date.parse(time.toString()));
-        return expect(behaviour.isActive).toBeTruthy();
-      });
-      test('Behaviour should be active when user is in sphere and activation between sunrise -10 offset -> sunset.', () => {
-        const behaviourSupport = new BehaviourSupport()
-        behaviourSupport.setTimeToSunset().setTimeFromSunrise(-10).setDimPercentage(90).setPresenceSomebodyInSphere();
-        const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
-        const time = new Date(2020,10,5,7,39);
-        behaviour.tick(Date.parse(time.toString()));
-        eventBus.emit(PRESENCE_DETECT_TOPIC, EVENT_ENTER_SPHERE);
-        behaviour.tick(Date.parse(time.toString()));
-        return expect(behaviour.isActive).toBeTruthy();
-      });
-      test('Behaviour should be inactive when user is in sphere and time is before activation between sunrise  -> sunset.', () => {
-        const behaviourSupport = new BehaviourSupport()
-        behaviourSupport.setTimeToSunset().setTimeFromSunrise().setDimPercentage(90).setPresenceSomebodyInSphere();
-        const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
-        const time = new Date(2020,10,5,7,48);
-        behaviour.tick(Date.parse(time.toString()));
-        eventBus.emit(PRESENCE_DETECT_TOPIC, EVENT_ENTER_SPHERE);
-        behaviour.tick(Date.parse(time.toString()));
-        return expect(behaviour.isActive).toBeFalsy();
-      });
-      test('Behaviour should be inactive when user is in sphere and time is after activation between sunrise  -> sunset + 10.', () => {
-        const behaviourSupport = new BehaviourSupport()
-        behaviourSupport.setTimeToSunset(+10).setTimeFromSunrise().setDimPercentage(90).setPresenceSomebodyInSphere();
-        const behaviour = new Behaviour(behaviourSupport.rule, sphereLocation);
-        const time = new Date(2020,10,5,17,18);
-        behaviour.tick(Date.parse(time.toString()));
-        eventBus.emit(PRESENCE_DETECT_TOPIC, EVENT_ENTER_SPHERE);
-        behaviour.tick(Date.parse(time.toString()));
-        return expect(behaviour.isActive).toBeFalsy();
-      });
-
-    })
+    });
   });
 });
+ 
